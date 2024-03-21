8b60ca5242afcf38919c7ecf2bada9b80b37ecd4
envymilo <nguyenviminhloc@gmail.com>
2024-03-21 18:35:20 +0700

    user

---
+++ src/app/management/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const management = () => {
+  return <></>;
+};
+
+export default management;
