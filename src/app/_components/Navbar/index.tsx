b2cea662a2e8265e221c4a22a72a787fddf8c7ee
envymilo <nguyenviminhloc@gmail.com>
2024-03-29 09:31:37 +0700

    123

--- src/app/_components/Navbar/index.tsx
+++ src/app/_components/Navbar/index.tsx
@@ -11,14 +11,15 @@ import {
 import { AuthModal } from "./AuthModal";
 import { AuthenticatedSelect } from "./AuthenticatedSelect";
 import { RootContext } from "@/app/_hooks/RootProvider";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export const Navbar = () => {
-  const { swrs }  = useContext(RootContext)!
-  const { profileSwr }  = swrs
-  const { data }  = profileSwr
+  const { swrs } = useContext(RootContext)!;
+  const { profileSwr } = swrs;
+  const { data } = profileSwr;
 
-  const router = useRouter()
+  const pathName = usePathname();
+  console.log(pathName);
 
   return (
     <NextUINavbar>
@@ -26,33 +27,27 @@ export const Navbar = () => {
         <p className="font-bold text-inherit">Orchid Auction</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
+        <NavbarItem isActive={pathName === "/"}>
           <Link color="foreground" href="/">
             Home
           </Link>
         </NavbarItem>
-        <NavbarItem isActive>
-          <Link onPress={() => router.push("/auctions")} aria-current="page">
+        <NavbarItem isActive={pathName === "/auctions"}>
+          <Link color="foreground" href="/auctions">
             Auctions
           </Link>
         </NavbarItem>
-        {
-          data?.role === "Administrator" ?
-          <NavbarItem>
-          <Link color="foreground" href="/management/requests">
-            Management
-          </Link>
-        </NavbarItem>
-        : null 
-        }
-       
-
+        {data?.role === "Administrator" ? (
+          <NavbarItem isActive={pathName === "/management/requests"}>
+            <Link color="foreground" href="/management/requests">
+              Management
+            </Link>
+          </NavbarItem>
+        ) : null}
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          {
-            data ? <AuthenticatedSelect /> : <AuthModal />
-          }            
+          {data ? <AuthenticatedSelect /> : <AuthModal />}
         </NavbarItem>
       </NavbarContent>
     </NextUINavbar>
--- src/app/management/layout.tsx
+++ src/app/management/layout.tsx
@@ -2,12 +2,14 @@
 
 import { BookUp, Flower, UserCircle } from "lucide-react";
 import Sidebar, { SidebarItem } from "./_components/sidebar";
+import { usePathname } from "next/navigation";
 
 const Layout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const pathName = usePathname();
   return (
     <>
       <div className="flex">
@@ -15,18 +17,20 @@ const Layout = ({
           <SidebarItem
             icon={<UserCircle size="20" />}
             text="User"
-            active
+            active={pathName === "/management/users"}
             href="/management/users"
           />
           <SidebarItem
             icon={<Flower size="20" />}
             text="Orchid"
             href="/management/orchids"
+            active={pathName === "/management/orchids"}
           />
           <SidebarItem
             icon={<BookUp size="20" />}
             text="Deposit Request"
             href="/management/requests"
+            active={pathName === "/management/requests"}
           />
         </Sidebar>
         {children}
